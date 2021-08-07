6b195846346e6368290f5f9f0982c2fe521209fc
galenus <galenus@users.noreply.github.com>
2021-08-07 22:23:15 +0300

    fix hooks and events types mapping args

--- src/processors/fields-processor.ts
+++ src/processors/fields-processor.ts
@@ -5,7 +5,7 @@ import {buildSignature} from "./callables-processor";
 export const EVENT_TYPE_NAME = "Event";
 
 export function buildTypedEvent(event: Event, parentDef: Context) {
-    return `${event.Name}: ${EVENT_TYPE_NAME}<${buildSignature(event, [parentDef], {
+    return `${event.Name}: ${EVENT_TYPE_NAME}<${buildSignature(event, [parentDef, event], {
         isStatic: true,
         isLambdaSignature: true
     })}>`;
@@ -14,7 +14,7 @@ export function buildTypedEvent(event: Event, parentDef: Context) {
 export const HOOK_TYPE_NAME = "Hook";
 
 export function buildTypedHook(hook: Hook, parentDef: Context) {
-    return `${hook.Name}: ${HOOK_TYPE_NAME}<${buildSignature(hook, [parentDef], {
+    return `${hook.Name}: ${HOOK_TYPE_NAME}<${buildSignature(hook, [parentDef, hook], {
         isStatic: true,
         isLambdaSignature: true
     })}>`;
